bed84bad2fc995764a7f78c881c3b47f85dac06a
c-jacs <116898006+c-jacs@users.noreply.github.com>
2023-11-13 11:28:16 -0500

    Added some docstrings to the interface file

--- interface.py
+++ interface.py
@@ -1,11 +1,17 @@
+""""""
 import tkinter as tk
 
-window = tk.Tk()
-greeting = tk.Label(text="Enter a song you like: ")
-entry = tk.Entry()
-greeting.pack()
-entry.pack()
+def enter_song():
+    """
+    Creates a window with a text box for the user to enter a song they like,
+    and saves that song to a variable to be used in the song recommendation program
+    """
+    window = tk.Tk()
+    greeting = tk.Label(text="Enter a song you like: ")
+    entry = tk.Entry()
+    greeting.pack()
+    entry.pack()
 
-song = entry.get()
+    song = entry.get()
 
-window.mainloop()
+    window.mainloop()
