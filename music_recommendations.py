daf9a86ef0e2c0b4108da3e9c7e3ead65118421c
c-jacs <116898006+c-jacs@users.noreply.github.com>
2023-11-13 09:49:24 -0500

    Created file and added the Spotify CSV I found.

---
+++ music_recommendations.py
@@ -0,0 +1,12 @@
+"""
+Ryan Boubsil, Eyosiyas Girma, Claire Jacobs
+Music Recommendation Program
+Professor Cruz
+INST 326 Final Project
+"""
+
+import pandas as pd
+import sys
+import argparse
+
+csv = pd.read_csv("spotify_songs")
