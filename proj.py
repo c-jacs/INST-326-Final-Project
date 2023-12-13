e8081440d75570030938897c9540abe8285c8306
c-jacs <116898006+c-jacs@users.noreply.github.com>
2023-12-13 09:54:26 -0500

    Added docstrings

--- proj.py
+++ proj.py
@@ -1,21 +1,63 @@
+"""
+Ryan Boubsil, Eyosiyas Girma, Claire Jacobs
+INST326 Final Project
+Song Recommendation Program
+15 November 2023
+"""
+
 import pandas as pd
 import argparse
 import sys
 
 class Song:
+    """Creates an object which stores details related to a given song.
 
+    Attributes:
+        name (str): The title of the song
+        artist (str): The artist of the song
+        genre (str): The song's primary genre
+        sub_genre (str): The song's secondary genre
+        energy (float): A perceptual measure of intensity and activity for the song
+    """
     def __init__(self, name, artist, genre, sub_genre, energy):
+        """Initializes a Song object.
+        
+        Args:
+            name (str): See class documentation
+            artist (str): See class documentation
+            genre (str): See class documentation
+            sub_genre (str): See class documentation
+            energy (float): See class documentation
+        """
         self.name = name
         self.artist = artist
         self.genre = genre
         self.sub_genre = sub_genre
         self.energy = energy
 
+
     def __str__(self):
+        """Returns the name and artist of the song object as a formatted string.
+        
+        Args:
+            None
+        """
         return f"{self.name} by {self.artist}"
-    
+
+
 def is_song_in_list(name, artist, file_path):
-    
+    """Reads in the csv attribute and checks the given song title and artist name
+    to make sure the base song exists in the dataset, and creates a Song object using
+    the name, artist, genre, subgenre, and energy associated with the song in the dataset.
+
+    Args:
+        name (str): The title of the given (base) song, read in from the terminal
+        artist (str): The name of the given song's artist, read in from the terminal
+        file_path (str): The path to the csv from which song data is being pulled, 
+            read in from the terminal
+    Returns:
+        song_in_list: Song object containing the details of the base song given by the user from the csv
+    """
     song_csv_to = pd.read_csv(file_path)
     song_csv = pd.DataFrame(song_csv_to)
     song_finder = song_csv[song_csv['Name'] == name]
@@ -40,15 +82,22 @@ def is_song_in_list(name, artist, file_path):
             energy_val = song_artist.iat[index_num, 4]
 
             song_in_list = Song(name_val, artist_val, genre_val, sub_genre_val, energy_val)
-            
-
+        
     return song_in_list
-
-                
 
 
 def recommend_song(name, artist, filepath):
+    """Uses the subgenre and energy attributes of the song_in_list object created in the 
+    is_song_in_list method to find another song from the dataset which has the same subgenre
+    and the closest energy level. If the base song was not found in the csv, returns an error message.
 
+    Args:
+        name (str): The name of the Song object returned from the is_song_in_list method
+        artist (str): The artist's name of the Song object returned from the is_song_in_list method 
+        filepath (str): The path to the csv from which song data is being pulled
+    Returns:
+        song_to_return: The recommended song that will be produced for the user
+    """
     if is_song_in_list(name, artist, filepath) is None:
         return "Song not found. Try another one!"
     else: 
@@ -88,9 +137,27 @@ def recommend_song(name, artist, filepath):
     
     
 def main(name, artist, filepath):
+    """Runs the recommend_song method.
+
+    Args:
+        name (str): name of the recommended song
+        artist (str): artist of the recommended song
+        filepath (str): path to the song csv from which song data will be pulled
+    Returns:
+        recommend_song 
+    """
     return recommend_song(name, artist, filepath)
 
+
 def parse_args(args_list):
+    """Takes a list of strings from the command prompt and passes them through as arguments.
+    
+    Args:
+        args_list (list) : the list of strings from the command prompt
+    Returns:
+        args (ArgumentParser)
+    """
+
     parser = argparse.ArgumentParser()
 
     parser.add_argument('song_name', type=str, help='Song name')
@@ -100,6 +167,7 @@ def parse_args(args_list):
     args = parser.parse_args(args_list)
 
     return args
+
 
 if __name__ == "__main__":
     parsed_args = parse_args(sys.argv[1:])
